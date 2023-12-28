85de85e1e79ecedd6dab433f1723bbc30e726a48
BDar01 <basim.dar@metu.edu.tr>
2023-12-28 10:24:44 +0000

    Confirm VS code works in Docker and with GitHub.

--- testVS.py
+++ testVS.py
@@ -1,2 +1,2 @@
-#Checking push from Docker to Git repo using VS Code
-print("VS code works in Docker.")
+#Checking push from Docker to Git repo using VS Code properly.
+print("VS code works in Docker fully.")
