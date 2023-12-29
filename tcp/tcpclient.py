de64e78ab40d5383f0bcdb0223da57372c7c0a0b
BDar01 <basim.dar@metu.edu.tr>
2023-12-29 15:09:11 +0000

    Made initial TCP client (seems to send correctly)

--- tcp/tcpclient.py
+++ tcp/tcpclient.py
@@ -1,23 +1,34 @@
 import socket
 import os
 
-def send_file():
+HOST = "172.17.0.2"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+def send_file(file_path, connection):
+    with open(file_path, 'rb') as file:
+        file_size = os.path.getsize(file_path)
+
+        # Send file size as a fixed-width string (e.g., 10 characters)
+        connection.sendall(str(file_size).ljust(10).encode())
+
+        data = file.read(1024)
+        while data:
+            connection.send(data)
+            data = file.read(1024)
+
+        print(f"File '{file_path}' sent successfully")
+
+def send_files():
     server_ip = '127.0.0.1'
     server_port = 12345
 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, server_port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
 
-    file_path = os.path.join(os.path.dirname(__file__), '..', 'objects', 'small-0.obj')  # Update the file path
-    with open(file_path, 'rb') as file:
-        data = file.read(1024)
-        while data:
-            client_socket.send(data)
-            data = file.read(1024)
+        file_paths = ['../../objects/large-0.obj', '../../objects/small-0.obj']
 
-    print(f"File '{file_path}' sent successfully")
-
-    client_socket.close()
+        for file_path in file_paths:
+            send_file(file_path, s)
 
 if __name__ == "__main__":
-    send_file()
+    send_files()
